517bc492063fed22bdd73201ac428c009f327237
Andrew Hulsizer <andrew@tastemade.com>
2017-05-03 22:15:19 -0500

    Initial

---
+++ Sources/Database.swift
@@ -0,0 +1,13 @@
+//
+//  Database.swift
+//  
+//
+//  Created by Andrew Hulsizer on 5/3/17.
+//
+//
+
+import Foundation
+
+struct Database {
+    
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import PostgreSQLTests
+
+XCTMain([
+    testCase(PostgreSQLTests.allTests),
+])
