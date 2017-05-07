78d235f7e3647a11a37931c479bf73054ef13ad4
Andrew Hulsizer <andrew@tastemade.com>
2017-05-07 15:36:18 -0400

    Update to add parse capabilities

--- Sources/Connection.swift
+++ Sources/Connection.swift
@@ -9,38 +9,51 @@
 import Foundation
 import libpq
 
-struct Connection {
+struct PostgresError: Error {
+    let message: String
+}
+
+final class Connection {
     
-    public let cConnection: OpaquePointer
+    public let connection: OpaquePointer
     public var isConnected: Bool {
-        if PQstatus(cConnection) == CONNECTION_OK {
+        if PQstatus(connection) == CONNECTION_OK {
             return true
         }
         return false
     }
     
-    public var lastError: String {
-        guard let errorMessage = PQerrorMessage(cConnection) else {
-            return ""
+    public func getError() -> PostgresError {
+        guard let errorMessage = PQerrorMessage(connection) else {
+            return PostgresError(message: "General Error")
         }
-        return String(cString: errorMessage)
+        return PostgresError(message: String(cString: errorMessage))
     }
     
-    init(name: String, user: String, password: String, port: String) throws {
-        let string = "host='localhost' port='\(port)' dbname='\(name)' user='\(user)' password='\(password)' client_encoding='UTF8'"
-        self.cConnection = PQconnectdb(string)
+    init(connection: String) throws {
+        self.connection = PQconnectdb(connection)
         if !isConnected {
-            throw DatabaseError.cannotEstablishConnection(lastError)
+            throw self.getError()
         }
+    }
+    
+    deinit {
+        PQclear(connection)
     }
     
     @discardableResult
     func execute(_ query: String, _ values: [Node]? = []) throws -> [[String: Node]] {
         
         guard !query.isEmpty else {
-            throw DatabaseError.noQuery
+            throw PostgresError(message: "Query cannot be empty")
         }
         
-        return []
+        let pointer = PQexec(connection, query)!
+        if let result = try Result(result: pointer) {
+            return result.parse()
+        }
+        
+        return [[:]]
+        
     }
 }
--- Sources/Result.swift
+++ Sources/Result.swift
@@ -7,7 +7,48 @@
 //
 
 import Foundation
+import libpq
 
-struct Result {
+final class Result {
     
+    public let result: OpaquePointer
+    
+    init?(result: OpaquePointer?) throws {
+        switch PQresultStatus(result) {
+        case PGRES_TUPLES_OK:
+            self.result = result!
+        case PGRES_COMMAND_OK:
+            return nil
+        default:
+            let message = PQresultErrorMessage(result)!
+            throw PostgresError(message: String(cString: message))
+        }
+    }
+    
+    deinit {
+        PQclear(result)
+    }
+    
+    func parse() -> [[String : Node]] {
+        
+        var parsed: [[String: Node]] = []
+        
+        let rowCount: Int32 = PQntuples(result)
+        let columnCount: Int32 = PQnfields(result)
+        
+        for row in 0..<rowCount {
+            
+            var item: [String: Node] = [:]
+            
+            for column in 0..<columnCount {
+                let name = String(cString: PQfname(result, column)!)
+                let value = PQgetvalue(result, row, column)!
+                
+                item[name] = Node.string(String(cString: value))
+                parsed.append(item)
+            }
+        }
+        
+        return parsed
+    }
 }
